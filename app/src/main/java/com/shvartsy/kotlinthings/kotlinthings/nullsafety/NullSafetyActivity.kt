c2007a8e6e5c0fbcab94b11c25eb8ce581897064
James Shvarts <jshvarts@gmail.com>
2017-06-10 09:56:35 -0400

    add null safety examples

--- app/src/main/java/com/shvartsy/kotlinthings/kotlinthings/nullsafety/NullSafetyActivity.kt
+++ app/src/main/java/com/shvartsy/kotlinthings/kotlinthings/nullsafety/NullSafetyActivity.kt
@@ -9,12 +9,45 @@ import com.shvartsy.kotlinthings.kotlinthings.R
  * Kotlin things related to Null Safety.
  */
 class NullSafetyActivity : AppCompatActivity() {
+
+    val notNullString: String = "NOT_NULL"
+
+    val nullString: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        println("enter Null Safety")
+        println("*** Null Safety ***")
+
+        printNullIfNull(notNullString)
+        printNullIfNull(nullString)
+
+        printNothingIfNull(notNullString)
+        printNothingIfNull(nullString)
+
+        printSuppliedDefaultIfNullUsingElvisOp(notNullString)
+        printSuppliedDefaultIfNullUsingElvisOp(nullString)
+
+        printAfterIfNullCheck(notNullString)
+        printAfterIfNullCheck(nullString)
+    }
+
+    fun printNullIfNull(str: String?) = println("printNullIfNull with param $str: " + str?.length)
+
+    fun printNothingIfNull(str: String?) = str?.let { println("printNothingIfNull with param $str: $it") }
+
+    fun printSuppliedDefaultIfNullUsingElvisOp(str: String?) {
+        val toPrint: String = str ?: "empty"
+        println("printSuppliedDefaultIfNullUsingElvisOp with param $str: $toPrint")
+    }
+
+    fun printAfterIfNullCheck(str: String?) {
+        if (str != null) {
+            // compiler tracks the null check and lets you access the value without the "?"
+            println("printAfterIfNullCheck with param $str: " + str.length)
+        }
     }
 }
