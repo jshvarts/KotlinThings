c85da5fd5f9795b5fd0b8e59906973458a26236d
James Shvarts <jshvarts@gmail.com>
2017-06-10 10:57:40 -0400

    add elvis operator example with return/throw

--- app/src/main/java/com/shvartsy/kotlinthings/kotlinthings/nullsafety/NullSafetyActivity.kt
+++ app/src/main/java/com/shvartsy/kotlinthings/kotlinthings/nullsafety/NullSafetyActivity.kt
@@ -22,21 +22,43 @@ class NullSafetyActivity : AppCompatActivity() {
 
         println("*** Null Safety ***")
 
+
         printNullIfNull(notNullString)
         printNullIfNull(nullString)
+
 
         printNothingIfNull(notNullString)
         printNothingIfNull(nullString)
 
+
         printSuppliedDefaultIfNullUsingElvisOp(notNullString)
         printSuppliedDefaultIfNullUsingElvisOp(nullString)
 
+
         printAfterIfNullCheck(notNullString)
         printAfterIfNullCheck(nullString)
+
+
+        println(": " + returnNullInRightHandElvisOp(notNullString))
+        println(": " + returnNullInRightHandElvisOp(nullString))
+
+
+        try {
+            println(": " + throwExceptionInRightHandElvisOp(notNullString))
+            println(": " + throwExceptionInRightHandElvisOp(nullString))
+        } catch (e: IllegalArgumentException) {
+            println(" threw exception: " + e)
+        }
     }
 
+    /**
+     * Produces null value instead of NullPointerException
+     */
     fun printNullIfNull(str: String?) = println("printNullIfNull with param $str: " + str?.length)
 
+    /**
+     * Performs a certain operation only for non-null values
+     */
     fun printNothingIfNull(str: String?) = str?.let { println("printNothingIfNull with param $str: $it") }
 
     fun printSuppliedDefaultIfNullUsingElvisOp(str: String?) {
@@ -49,5 +71,25 @@ class NullSafetyActivity : AppCompatActivity() {
             // compiler tracks the null check and lets you access the value without the "?"
             println("printAfterIfNullCheck with param $str: " + str.length)
         }
+    }
+
+    /**
+     * Since return is an expression in Kotlin, it can be used on the right hand side of the elvis operator.
+     * Handy for checking function arguments
+     */
+    fun returnNullInRightHandElvisOp(str: String?): String? {
+        print("returnNullInRightHandElvisOp with param: " + str)
+        val ret = str ?: return null
+        return ret
+    }
+
+    /**
+     * Since throw is an expression in Kotlin, it can be used on the right hand side of the elvis operator.
+     * Handy for checking function arguments
+     */
+    fun throwExceptionInRightHandElvisOp(str: String?): String? {
+        print("throwExceptionInRightHandElvisOp with param: " + str)
+        val ret = str ?: throw IllegalArgumentException("str expected")
+        return ret
     }
 }
